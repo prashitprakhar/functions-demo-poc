ae6166821a6030c9263bc7fd5194b60e3a4d8438
prashitprakhar <32621695+prashitprakhar@users.noreply.github.com>
2018-01-03 11:02:44 +0530

    Updating code for REST API functions for HttpTriggerJS Function

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,10 +1,75 @@
-module.exports = function (context, req) {
+function tableRouter(context, req) {
+    var res = context.res;
+    var id = context.bindings.id;
+ 
+    switch (req.method) {
+        case 'GET':
+            if (id) {
+                getOneItem(req, res, id);
+            } else {
+                getAllItems(req, res);
+            }
+            break;
+ 
+        case 'POST':
+            insertItem(req, res);
+            break;
+ 
+        case 'PATCH':
+            patchItem(req, res, id);
+            break;
+ 
+        case 'PUT':
+            replaceItem(req, res, id);
+            break;
+ 
+        case 'DELETE':
+            deleteItem(req, res, id);
+            break;
+ 
+        default:
+            res.status(405).json({ error: "Operation not supported", message: `Method ${req.method} not supported`})
+    }
+}
+ 
+function getOneItem(req, res, id) {
+    res.status(200).json({ id: id, message: "getOne" });
+}
+ 
+function getAllItems(req, res) {
+    res.status(200).json({ query: req.query, message: "getAll" });
+}
+ 
+function insertItem(req, res) {
+    res.status(200).json({ body: req.body, message: "insert"});
+}
+ 
+function patchItem(req, res, id) {
+    res.status(405).json({ error: "Not Supported", message: "PATCH operations are not supported" });
+}
+ 
+function replaceItem(req, res, id) {
+    res.status(200).json({ body: req.body, id: id, message: "replace" });
+}
+ 
+function deleteItem(req, res, id) {
+    res.status(200).json({ id: id, message: "delete" });
+}
+ 
+module.exports = tableRouter;
+
+
+
+
+
+
+/* module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hi " + (req.query.name || req.body.name)
+          /*   body: "Hi " + (req.query.name || req.body.name)
         };
     }
     else {
@@ -14,4 +79,4 @@ module.exports = function (context, req) {
         };
     }
     context.done();
-};
+};  */
